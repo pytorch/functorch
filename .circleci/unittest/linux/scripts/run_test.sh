1956406555af20a8319e0f11c999e03551e66d31
Sam Andow <samdow@fb.com>
2021-10-08 17:19:26 +0000

    run without parallelization

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -9,4 +9,4 @@ python -m torch.utils.collect_env
 
 # test_functorch_lagging_op_db.py: Only run this locally because it checks
 # the functorch lagging op db vs PyTorch's op db.
-pytest -v --ignore=test/test_functorch_lagging_op_db.py test/ || exit 255
+pytest -n 0 -v --ignore=test/test_functorch_lagging_op_db.py test/ || exit 255
