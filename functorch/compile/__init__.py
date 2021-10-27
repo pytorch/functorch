fb1ad448494274bfd0d3b21a6a3b6a4fdcc99d0f
Richard Zou <zou3519@gmail.com>
2021-10-27 10:41:40 -0400

    wip

--- functorch/compile/__init__.py
+++ functorch/compile/__init__.py
@@ -1,1 +1,4 @@
 from .._src.operator_authoring import pointwise_operator
+from .._src.eager_compilation import compiled_function, compiled_module, tvm_compile, draw_joint_graph, default_partition, partition_with_recompute_fwd_in_bwd
+from .._src.python_key import nnc_jit, make_nnc
+from .._src.nnc_compile import nnc_compile, get_ops
