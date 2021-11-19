eb84cd33a30f82b53515ef45c2bbfbecd6642b8d
Horace He <horacehe2007@yahoo.com>
2021-11-19 00:53:41 -0800

    Added decomposition testing + display infra

--- functorch/_src/python_key.py
+++ functorch/_src/python_key.py
@@ -13,10 +13,12 @@ import torch.utils._pytree as pytree
 from torch.fx import Tracer, GraphModule
 import torch.fx as fx
 import torch.fx._pytree as fx_pytree
+from torch import Tensor
 from .nnc_compile import nnc_compile
 from enum import Enum
 import warnings
 from contextlib import contextmanager
+import math
 aten = torch.ops.aten
 
 decomposition_table = {}
@@ -27,32 +29,68 @@ def register_decomposition(aten_op):
         return f
     return decomposition_decorator
 
+class Reduction(Enum):
+    NONE = 0
+    MEAN = 1
+    SUM = 2
+
 @register_decomposition(aten.tanh_backward)
-def tanh_backward_decomposition(out_grad, y):
+def tanh_backward_decomposition(out_grad: Tensor, y: Tensor):
     return aten.sub(out_grad, out_grad * y * y)
 
 @register_decomposition(aten.sigmoid_backward)
-def sigmoid_backward_decomposition(out_grad, y):
+def sigmoid_backward_decomposition(out_grad: Tensor, y: Tensor):
     return out_grad * (y * (1 - y))
 
-@register_decomposition(aten._s_where)
-def _s_where_decomposition(a, b, c):
-    return aten.where(a, b, c)
-
 @register_decomposition(aten.detach)
-def detach_decomposition(x):
+# This is valid if we always sit behind autograd
+def detach_decomposition(x: Tensor):
     return x
 
 @register_decomposition(aten.softplus_backward)
-def softplus_backward_decomposition(out_grad, x, beta, threshold, out):
+# The out argument seems to always be ignored?
+def softplus_backward_decomposition(out_grad: Tensor, x: Tensor, beta: float, threshold: float, out):
     z = (x * beta).exp()
     return aten.where((x * beta) > threshold, out_grad, out_grad * z / (z + 1.0))
+
+@register_decomposition(aten.elu_backward)
+def elu_backward_decomposition(grad_output: Tensor, alpha: float, scale: float, input_scale: float, is_result: bool, self_or_result: Tensor):
+    negcoef = alpha * scale
+    poscoef = scale
+    negiptcoef = input_scale
+    if is_result:
+        return aten.where(b <= 0, grad_output * negiptcoef * (self_or_result + negcoef), self_or_result * poscoef)
+    else:
+        return aten.where(self_or_result <= 0, grad_output * negiptcoef * negcoef * aten.exp(self_or_result * negiptcoef), grad_output * poscoef)
+
+@register_decomposition(aten.hardsigmoid_backward)
+def hardsigmoid_backward_decomposition(grad_output: Tensor, self: Tensor):
+    return aten.where((self > -3.0) & (self < 3.0), grad_output * (1.0/6.0), torch.tensor(0.0))
+
+@register_decomposition(aten.hardtanh_backward)
+def hardtanh_backward_decomposition(grad_output: Tensor, self: Tensor, min_val: float, max_val: float):
+    return aten.where((self <= min_val) | (self >= max_val), torch.tensor(0.0, dtype=grad_output.dtype), grad_output)
+
+@register_decomposition(aten.threshold_backward)
+def threshold_backward_decomposition(grad_output: Tensor, self: Tensor, threshold: float):
+    return aten.where(self <= threshold, torch.tensor(0.0, dtype=grad_output.dtype), grad_output)
+
+@register_decomposition(aten.mse_loss_backward)
+def mse_loss_backward_decomposition(grad_output: Tensor, input: Tensor, target: Tensor, reduction: int):
+    norm = 2./input.numel() if reduction == Reduction.MEAN.value else 2.
+    return norm * (input - target) * grad_output
 
 # @register_decomposition(aten._fused_dropout)
 # def _fused_dropout_decomposition(input, p, generator=None):
 #     mask = aten.to(aten.rand_like(input) < p, dtype=torch.uint8)
 #     res = mask.type_as(input) * input * (1./p)
 #     return [res, mask]
+
+
+# These all suck
+@register_decomposition(aten._s_where)
+def _s_where_canonicalization(a, b, c):
+    return aten.where(a, b, c)
 
 USE_DECOMPOSE = False
 
