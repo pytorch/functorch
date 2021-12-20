6e4c2e6be907acd89a08d73fd23ae908e9527551
kshitij12345 <kshitijkalambarkar@gmail.com>
2021-12-20 06:43:37 +0000

    remove tree_flatten_hack and update

--- functorch/_src/pytree_hacks.py
+++ functorch/_src/pytree_hacks.py
@@ -7,41 +7,6 @@
 import torch.utils._pytree as _pytree
 from torch.utils._pytree import tree_flatten, tree_unflatten, TreeSpec
 from typing import List, Any
-import torch
-import inspect
-
-
-for name in dir(torch.return_types):
-    if name.startswith('__'):
-        continue
-    attr = getattr(torch.return_types, name)
-    if inspect.isclass(attr):
-        return_type_class = attr
-        # Note: We capture the current `return_type_class` with default argument `constructor`
-        # in the lambda otherwise we will point to the last value of `return_type_class` for all lambdas
-        _pytree._register_pytree_node(return_type_class, lambda x: (
-            tuple(x), None), lambda x, c, constructor=return_type_class: constructor(x))
-
-
-# TODO: The following function should only be used with vmap.
-def tree_flatten_hack(pytree):
-    if _pytree._is_leaf(pytree) and not isinstance(pytree, tuple):
-        return [pytree], _pytree.LeafSpec()
-
-    typ = type(pytree)
-
-    flatten_fn = _pytree.SUPPORTED_NODES[typ].flatten_fn
-    child_pytrees, context = flatten_fn(pytree)
-
-    # Recursively flatten the children
-    result: List[Any] = []
-    children_specs: List['TreeSpec'] = []
-    for child in child_pytrees:
-        flat, child_spec = tree_flatten_hack(child)
-        result += flat
-        children_specs.append(child_spec)
-
-    return result, _pytree.TreeSpec(typ, context, children_specs)
 
 
 def tree_map_(fn_, pytree):
