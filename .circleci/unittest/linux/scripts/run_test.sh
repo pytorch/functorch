1dffa558b7a880ab91037dc8a009f4ea8f0d0d0a
Sam Andow <samdow@fb.com>
2022-02-15 17:09:08 +0000

    get testing times

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -12,5 +12,5 @@ python -m torch.utils.collect_env
 # test_functorch_lagging_op_db.py: Only run this locally because it checks
 # the functorch lagging op db vs PyTorch's op db.
 EXIT_STATUS=0
-find test \( -name test\*.py ! -name test_functorch_lagging_op_db.py \) | xargs -I {} -n 1 python {} -v || EXIT_STATUS=$?
+cat tests.txt | xargs -I {} -n 1 python test/test_ops.py -k {} -v || EXIT_STATUS=$?
 exit $EXIT_STATUS
