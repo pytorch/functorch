0e1afba946942395399885c67e2377c9ae889647
vfdev-5 <vfdev.5@gmail.com>
2022-02-22 23:42:53 +0000

    Updated the way to decompose trace_backward and added a test

--- test/test_vmap.py
+++ test/test_vmap.py
@@ -2975,6 +2975,17 @@ class TestVmapBatchedGradient(Namespace.TestVmapBase):
         x = torch.randn(2, 3, device=device, requires_grad=True)
         self._batched_grad_test(Tensor.trace, (x,))
 
+        # TODO: put this test to where it will be appropriate
+        from functorch import grad
+
+        x = torch.randn(3, 2, 2, device=device)
+
+        def sum_grad_trace(x):
+            return grad(torch.trace)(x).sum()
+
+        output = vmap(grad(sum_grad_trace))(x)
+        self.assertEqual(output, torch.zeros_like(output))
+
     @skipCUDAIfNoMagma
     @allowVmapFallbackUsage
     def test_symeig(self, device):
