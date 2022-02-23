99e682fb62db3b3d83bfb2506dab1f3b5c8cd0c8
Samantha Andow <samdow@fb.com>
2022-02-23 17:42:08 -0500

    Update run_test.sh

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -12,5 +12,5 @@ python -m torch.utils.collect_env
 # test_functorch_lagging_op_db.py: Only run this locally because it checks
 # the functorch lagging op db vs PyTorch's op db.
 EXIT_STATUS=0
-cat tests.txt | xargs -I {} -n 1 python test/test_ops.py -k {} -v || EXIT_STATUS=$?
+python test/test_ops.py -k 'test_vmapvjpvjp' -v || EXIT_STATUS=$?
 exit $EXIT_STATUS
