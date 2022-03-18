a98ba92e98af789f205a4a6c88dc5b4f2f19fd1a
Richard Zou <zou3519@gmail.com>
2022-03-17 19:58:32 -0700

    [release] Bump version

--- setup.py
+++ setup.py
@@ -26,10 +26,11 @@ except Exception:
     sha = 'Unknown'
 package_name = 'functorch'
 
-# if os.getenv('BUILD_VERSION'):
-#     version = os.getenv('BUILD_VERSION')
-# elif sha != 'Unknown':
-#     version += '+' + sha[:7]
+# TODO: comment the following right before release
+if os.getenv('BUILD_VERSION'):
+    version = os.getenv('BUILD_VERSION')
+elif sha != 'Unknown':
+    version += '+' + sha[:7]
 
 def write_version_file():
     version_path = os.path.join(cwd, 'functorch', 'version.py')
