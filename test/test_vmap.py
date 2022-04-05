d6110f8903631373e170ba83cb1bc423c444ee5e
Sam Andow <samdow@fb.com>
2022-04-05 21:30:09 +0000

    normal_fix

--- test/test_vmap.py
+++ test/test_vmap.py
@@ -3802,7 +3802,7 @@ class TestRandomness(TestCase):
             lambda t, _: t.random_(**kwargs),
             lambda t, _: t.random_(100, **kwargs),
             lambda t, _: t.random_(-5, 100, **kwargs),
-            # lambda t, _: t.normal_(**kwargs),  TODO(samdow): fix normal_ with -1 bdim
+            lambda t, _: t.normal_(**kwargs),
             lambda t, _: t.bernoulli_(**kwargs),
             lambda t, _: t.cauchy_(**kwargs),
             lambda t, _: t.exponential_(**kwargs),
@@ -3839,7 +3839,7 @@ class TestRandomness(TestCase):
                 self.assertEqual(vmap_result, expected)
             else:
                 if batched_input != "none":
-                    passed_expected = passed_expected[0]
+                    passed_expected = passed_expected[0].clone()  # bug in pytorch, normal_ on views doesn't work
                 expected = op(passed_expected, always_batched)
                 self._assert_all_slices_equal(vmap_result)
                 for i in range(B0):
