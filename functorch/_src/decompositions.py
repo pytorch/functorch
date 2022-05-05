0bda7c7c6684f3a6456f0a96c6bbddca82e7ed25
Horace He <chilli@fb.com>
2022-05-05 21:59:26 +0000

    move some other stuff and port some other stuff

--- functorch/_src/decompositions.py
+++ functorch/_src/decompositions.py
@@ -8,42 +8,15 @@ from torch.utils._pytree import tree_map
 import torch._decomp
 
 aten = torch.ops.aten
-aten.__origin__ = None
 
 decomposition_table = torch._decomp.decomposition_table
+register_decomposition = torch._decomp.register_decomposition
+get_decompositions = torch._decomp.get_decompositions
 
 
-def register_decomposition(aten_op, registry=None):
-    def decomposition_decorator(f):
-        nonlocal registry
-        if registry is None:
-            registry = decomposition_table
+@register_decomposition(aten.trace.default)
+def trace(self: Tensor) -> Tensor:
+    return torch.sum(torch.diag(self))
 
-        def add_op_to_table(aten_op):
-            # Converts aten.foo to aten.foo.default
-            # Done so I can be lazy and not write default on all of these ops
-            if not isinstance(aten_op, torch._ops.OpOverload):
-                op_overload = aten_op.default
-            else:
-                op_overload = aten_op
-            registry[op_overload] = f
-        # To handle allowing multiple aten_ops at once
-        tree_map(add_op_to_table, aten_op)
-        return f
-    return decomposition_decorator
+# Decompositions have been ported to torch._decomp inside of PyTorch core. The only decompositions here are temporary or hacks. Please provide your contributions to PyTorch core!
 
-
-def get_decompositions(aten_ops: List[torch._ops.OpOverload]):
-    packets_to_overloads = defaultdict(list)
-    for op in decomposition_table:
-        packets_to_overloads[op.overloadpacket].append(op)
-    decompositions = {}
-    for op in aten_ops:
-        if op in packets_to_overloads:
-            for op_overload in packets_to_overloads[op]:
-                decompositions[op_overload] = decomposition_table[op_overload]
-        elif op in decomposition_table:
-            decompositions[op] = decomposition_table[op]
-    return decompositions
-
-# Decompositions have been ported to torch._decomp inside of PyTorch core. Please port contributions there!
