ab44cade4b219523ee34c44430eb5e561e2f8aad
cyy <cyyever@outlook.com>
2022-05-12 15:08:02 +0800

    mordenize code

--- functorch/csrc/CustomFunction.cpp
+++ functorch/csrc/CustomFunction.cpp
@@ -22,7 +22,7 @@ public:
   // py::object destructor.  This is because this object may outlive
   // libtorch_python, so we want to disarm the deallocation if that happens.
   // PyInterpreter does this correctly, pybind11 does not.
-  ~PythonKernelHolder() {
+  ~PythonKernelHolder() override {
     getPyInterpreter()->decref(func_, /*is_tensor*/false);
   }
 
@@ -201,7 +201,7 @@ variable_list GenericPythonBackward::apply(variable_list&& grads) {
   return grad_inputs;
 }
 
-typedef TensorList (*custom_python_function_t)(TensorList);
+using custom_python_function_t = TensorList (*)(TensorList);
 
 using torch::autograd::compute_requires_grad;
 using torch::autograd::collect_next_edges;
@@ -246,7 +246,7 @@ void customFunctionBoxed(const c10::OperatorHandle& op, torch::jit::Stack* stack
       if (!is_input) {
         set_history(tensor, grad_fn);
       }
-      grad_fn->saved_tensors_.push_back(torch::autograd::SavedVariable(tensor, !is_input));
+      grad_fn->saved_tensors_.emplace_back(tensor, !is_input);
     }
   }
   torch::jit::push(stack, result);
