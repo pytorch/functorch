a0169a26fc6d1d048c97491e6a0c138e5655804e
Shangdi Yu <yushangdi123@gmail.com>
2022-06-03 17:48:58 +0000

    benchmark

--- cse/benchmark.py
+++ cse/benchmark.py
@@ -6,6 +6,7 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 from cse import modify
 
+# @torch.jit.script
 # def f(x):
 #     vals = [x]
 #     ops = [torch.clone, torch.cos, torch.tanh, torch.nn.functional.gelu]
@@ -14,22 +15,44 @@ from cse import modify
 #         vals.append(new_val)
 #     return vals[-1]
 
-# @torch.jit.script
+
 # def f(x):
-#  return x.cos().cos()
+#     a = x.sum()
+#     b = x.sum()
+#     c = x.sum()
+#     d = x.sum()
+#     return a + b + c + d
 
-# inp = torch.randn(2**20, device='cuda')
-# for _ in range(5):
-#  f(inp)
+def profile_it(f, inp):
+    for _ in range(5):
+        f(inp)
 
-# with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
-#     for _ in range(5):
-#     f(inp)
+    with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+        for _ in range(5):
+            f(inp)
 
-# print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
-
-
-# new_g = torch.jit.script(new_g)
-# new_g(t)
+    timing = prof.key_averages().table(sort_by="cuda_time_total", row_limit=10)
+    print(type(timing))
+    print(timing)
 
 
+def f(x):
+ return x.cos().cos()
+
+inp = torch.randn(2**20, device='cuda')
+
+fx_g =  make_fx(f)(inp)
+script_f = torch.jit.script(fx_g)
+new_g = modify(fx_g.graph)
+new_g = fx.GraphModule(fx_g, new_g)
+script_g = torch.jit.script(new_g)
+
+
+profile_it(script_f, inp)
+profile_it(script_g, inp)
+
+
+# print(type(fx_g))
+# fx_g = fx.symbolic_trace(f)
+# fx_g.graph.eliminate_dead_code()
+# fx_g.recompile()
