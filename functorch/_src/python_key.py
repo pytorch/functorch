131e3a6f4a1823c214ef6c2bd2bd97c9600f4770
Horace He <chilli@fb.com>
2022-06-07 08:20:33 +0000

    fix lint issues

--- functorch/_src/python_key.py
+++ functorch/_src/python_key.py
@@ -3,14 +3,5 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-import functools
-from typing import Any, Dict, Optional, Tuple, Callable, Union
-import torch
-from torch._C import _disabled_torch_function_impl
-import torch.utils._pytree as pytree
-from torch.fx import Tracer, GraphModule
-import torch.fx as fx
-from torch.fx.passes.shape_prop import _extract_tensor_metadata
-from contextlib import contextmanager
-
+__all__ = ["make_fx", "ProxyTensor", "dispatch_trace", "PythonKeyTracer"]
 from torch.fx.experimental.proxy_tensor import make_fx, ProxyTensor, dispatch_trace, PythonKeyTracer
