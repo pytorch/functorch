d97815a8063c2bc228287a13f103633d47bcff55
Richard Zou <zou3519@gmail.com>
2022-06-13 13:27:24 -0700

    [release] fix CUDA CI

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -56,6 +56,8 @@ if [ "${CU_VERSION:-}" == cpu ] ; then
     pip install torch~=1.12 torchvision -f https://download.pytorch.org/whl/test/cpu/torch_test.html --pre
     PYTORCH_VERSION="$(python -c "import torch; print(torch.__version__)")" python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 else
-    pip install torch~=1.12 torchvision -f https://download.pytorch.org/whl/test/cu102/torch_test.html --pre
+    # pip install torch~=1.12 torchvision -f https://download.pytorch.org/whl/test/cu102/torch_test.html --pre
+    # Workaround pip wheel cuda linking issue
+    conda install -y pytorch torchvision cudatoolkit=10.2 -c pytorch-test
     PYTORCH_VERSION="$(python -c "import torch; print(torch.__version__)")" python setup.py develop bdist_wheel -d $WHEELS_FOLDER
 fi
