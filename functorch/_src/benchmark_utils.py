3d98ef3fc714f03e5a3cf4121ea3e56edeeb812e
Shangdi Yu <yushangdi123@gmail.com>
2022-06-14 16:22:03 +0000

    fix lint

--- functorch/_src/benchmark_utils.py
+++ functorch/_src/benchmark_utils.py
@@ -4,14 +4,18 @@ import time
 import torch
 from torch.profiler import profile
 
+
 def synchronize():
     pass
+
 
 def dump_chrome_trace(f, input, trace_filename, optimize_ctx, activities, num_runs=1, randomize_input=False,
                       devices=None, kwargs_for_f=None, kwargs_for_profiler=None):
     """
-    Output the chrome trace of running f(input, **kwargs_for_f) with [optimize_ctx] [num_runs] times to [trace_filename].
-    [activities] are the activities that the profiler will record. e.g. ProfilerActivity.CUDA
+    Output the chrome trace of running f(input, **kwargs_for_f) with [optimize_ctx] 
+    [num_runs] times to [trace_filename].
+    
+    [activities] are the activities that the profiler will record, e.g. ProfilerActivity.CUDA.
     Return total runtime without the profiler
 
     Outputs to trace_filename
@@ -24,10 +28,10 @@ def dump_chrome_trace(f, input, trace_filename, optimize_ctx, activities, num_ru
     if devices != ["cpu"] and torch.cuda.is_available():
         synchronize = torch.cuda.synchronize
 
-    inputs = (
+    input = (
         randomize_input(copy.deepcopy(input))
         if randomize_input
-        else inputs
+        else input
     )
 
     if kwargs_for_f is None:
@@ -38,7 +42,7 @@ def dump_chrome_trace(f, input, trace_filename, optimize_ctx, activities, num_ru
     with optimize_ctx:
         torch.manual_seed(1337)
         for _ in range(5):  # warmup runs
-            f(inputs, **kwargs_for_f)
+            f(input, **kwargs_for_f)
             synchronize()
         torch.manual_seed(1337)
         t0 = time.perf_counter()
