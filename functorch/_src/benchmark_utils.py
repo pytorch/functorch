e325d0745487e037565c152bde82932ae9d85f9b
Shangdi Yu <yushangdi123@gmail.com>
2022-06-14 00:51:20 +0000

    add input shape

--- functorch/_src/benchmark_utils.py
+++ functorch/_src/benchmark_utils.py
@@ -4,6 +4,7 @@ import sys
 import logging
 import copy
 import time
+import pickle
 
 import torch
 from torch.profiler import profile, ProfilerActivity
@@ -17,7 +18,13 @@ folder_name = ""
 def _save_fx(gm, example_inputs):
     from functorch.compile import aot_module_simplified
 
-    def graph_saver_forward(gm, _):
+    def graph_saver_forward(gm, fw_args):
+        input_meta = []
+        for arg in fw_args:
+            if(type(arg) == int or type(arg) == float):
+                input_meta.append((type(arg),))
+            else:
+                input_meta.append((type(arg), arg.shape, arg.stride(), arg.dtype))
         global current_name
         global graph_index
         global folder_name
@@ -25,12 +32,20 @@ def _save_fx(gm, example_inputs):
         if not isExist:
             os.makedirs(f"{folder_name}/{current_name}")
         gm.to_folder(f"{folder_name}/{current_name}/{current_name}_forward_{graph_index}")
+        pickle.dump(input_meta, open( f"{folder_name}/{current_name}/{current_name}_forward_{graph_index}/{current_name}_forward_{graph_index}.input", "wb" ))
         return gm
 
-    def graph_saver_backward(gm, _):
+    def graph_saver_backward(gm, bw_args):
+        input_meta = []
+        for arg in bw_args:
+            if(type(arg) == int or type(arg) == float):
+                input_meta.append((type(arg),))
+            else:
+                input_meta.append((type(arg), arg.shape, arg.stride(), arg.dtype))
         global current_name
         global graph_index
         gm.to_folder(f"{folder_name}/{current_name}/{current_name}_backward_{graph_index}")
+        pickle.dump(input_meta, open( f"{folder_name}/{current_name}/{current_name}_backward_{graph_index}/{current_name}_backward_{graph_index}.input", "wb" ))
         graph_index = graph_index + 1
         return gm
 
@@ -43,6 +58,8 @@ def save_fx_graph(f, input, graph_name, _folder_name, manual_seed = 1337):
     The forward/backward computation graph of f will be stored in 
     {folder_name}/{current_name}/{current_name}_forward_{graph_index} and 
     {folder_name}/{current_name}/{current_name}_backward_{graph_index} respectively.
+    The input shape of the graphs will be stored in the .input files. These files can be loaded with pickle,
+    and is a list of format (type, shape, stride, dtype). In the case of type = int or float, it is just (type,).
 
     Since each f might produce multiple graphs, the graph_index is used to distinguish difference graphs
     """
