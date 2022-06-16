e83925a356b5d74c3e6e246bdc1eea4823d2b176
Shangdi Yu <yushangdi123@gmail.com>
2022-06-16 18:17:19 +0000

    setup benchmark

--- benchmarks/benchmark_remat.py
+++ benchmarks/benchmark_remat.py
@@ -10,6 +10,49 @@ import operator
 
 from functorch._src.remat_utils import rematerialize
 
+
+
+
+def benchmark_GPU_time(f, inp, itr = 5):
+
+    inp = torch.randn(2**22, device='cuda')
+    for _ in range(5):
+        f(inp)
+    with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+        for _ in range(itr):
+            f(inp)
+
+    timing = prof.key_averages()
+    cuda_time_total = 0
+    for e in timing:
+        cuda_time_total = cuda_time_total + e.cuda_time_total
+    return cuda_time_total / itr
+    # print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+
+def profile_function(name, f, inp):
+    traced_graph = make_fx(f, decomposition_table={torch.ops.aten.detach.default: lambda x: x})(torch.randn(2))
+
+    script_f = torch.jit.script(traced_graph)
+    avg_cuda_time_f = benchmark_GPU_time(script_f, inp)
+
+    fused_graph = rematerialize(traced_graph)
+
+    for node in fused_graph.graph.nodes:
+        if "fused_" in node.name:
+            module = getattr(fused_graph, node.name)
+            setattr(fused_graph, node.name, torch.jit.script(module) )
+
+    # fused_graph.fused_1 = torch.jit.script(fused_graph.fused_1)
+    # fused_graph.fused_0 = torch.jit.script(fused_graph.fused_0)
+
+    avg_cuda_time_g = benchmark_GPU_time(fused_graph, inp)
+
+    print(f"{name}, {avg_cuda_time_f}, {avg_cuda_time_g}")
+
+g_gpu = torch.Generator(device='cuda')
+g_gpu.manual_seed(2147483647)
+inp = torch.randn(2**20, device='cuda', generator=g_gpu)
+
 def f(a):
     b = a.cos()
     c = torch.relu(b)
@@ -18,21 +61,4 @@ def f(a):
     f = torch.relu(e)
     return b + c + e + f
 
-traced_graph = make_fx(f, decomposition_table={torch.ops.aten.detach.default: lambda x: x})(torch.randn(2))
-fused_graph = rematerialize(traced_graph)
-
-a = torch.rand(5)
-expected = f(a)
-result = fused_graph(a)
-torch.testing.assert_close(expected, result)
-
-fused_graph.fused_1 = torch.jit.script(fused_graph.fused_1)
-fused_graph.fused_0 = torch.jit.script(fused_graph.fused_0)
-
-inp = torch.randn(2**22, device='cuda')
-for _ in range(5):
-    fused_graph(inp)
-with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
-    for _ in range(5):
-        fused_graph(inp)
-print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+profile_function("f", f, inp)
