b93d33d1a0963b6b21a3331b878557e23fd7f984
Shangdi Yu <yushangdi123@gmail.com>
2022-06-20 20:48:13 +0000

    start weighted version

---
+++ functorch/_src/utilities.py
@@ -0,0 +1,32 @@
+import torch
+
+def _prod(x):
+    s = 1
+    for i in x:
+        s *= i
+    return s
+
+
+def _size_of(metadata):
+    sizes = {
+        torch.float: 4,
+        torch.float16: 2,
+        torch.bfloat16: 2,
+        torch.float32: 4,
+        torch.float64: 8,
+        torch.int: 4,
+        torch.int8: 1,
+        torch.int16: 2,
+        torch.int32: 4,
+        torch.int64: 8,
+        torch.uint8: 1,
+        torch.bool: 1,
+    }
+
+    numel = _prod(metadata.shape)
+    dtype = metadata.dtype
+
+    if dtype not in sizes:
+        raise NotImplementedError("Don't know the size of dtype ", dtype)
+
+    return numel * sizes[dtype]
