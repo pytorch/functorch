e43407fbbc286817f0ecd8c3b1086ce920eae3f2
Shangdi Yu <yushangdi123@gmail.com>
2022-06-28 17:09:03 +0000

    add joint benchmark

--- benchmarks/benchmark_remat.py
+++ benchmarks/benchmark_remat.py
@@ -9,10 +9,16 @@ from torch.fx._symbolic_trace import symbolic_trace
 import pickle
 import copy
 
-from functorch._src.remat_utils import rematerialize, get_fused_graph, rematerialize_fused_graph
+from functorch._src.remat_utils_mincut import rematerialize, get_fused_graph, rematerialize_fused_graph
 from functorch._src.cse import fx_graph_cse
 
 random.seed(1)
+
+
+def _prepare_for_jit_script(gm):
+    for node in gm.graph.nodes:
+        if isinstance(node.target, torch._ops.OpOverload):
+            node.target = node.target.overloadpacket
 
 def benchmark_GPU_time(f, inp, list_inp, itr = 5):
     if list_inp:
@@ -97,6 +103,8 @@ def profile_rematerialize(traced_graph, inp, list_inp):
 
 def profile_function(name, f, inp, list_inp = False):
     traced_graph = make_fx(f, decomposition_table={torch.ops.aten.detach.default: lambda x: x})(inp)
+    _prepare_for_jit_script(traced_graph)
+
     avg_cuda_time_f = profile_graph(traced_graph, inp, list_inp)
 
     csed = fx_graph_cse(traced_graph.graph)
@@ -105,7 +113,7 @@ def profile_function(name, f, inp, list_inp = False):
 
     avg_cuda_time_g, num_fused_group = profile_fused(csed_graph, inp, list_inp)
     avg_cuda_time_h, _ = profile_rematerialize(csed_graph_copy, inp, list_inp)
-    print(f"{name}, {avg_cuda_time_f}, {avg_cuda_time_g}, {avg_cuda_time_h}, {num_fused_group}")
+    print(f"{name}, {avg_cuda_time_f}, {avg_cuda_time_g}, {avg_cuda_time_h}, {num_fused_group}", flush=True)
 
 
 def f(a):
@@ -135,49 +143,22 @@ inp = torch.randn(2**20, device='cuda', generator=g_gpu)
 
 print("name, scripted_cuda_time, fused_cuda_time, remat_cuda_time, num_fused_group")
 
-profile_function("f", f, inp)
+# profile_function("f", f, inp)
 
-for i in range(10):
-    profile_function(f"rand_test_{i}", frandom, inp)
+# for i in range(10):
+#     profile_function(f"rand_test_{i}", frandom, inp)
 
-
-class FxModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, primals_1, primals_2, primals_3, primals_4, primals_5, primals_6, primals_7, primals_8, primals_9, primals_10, primals_11, primals_12, primals_13, primals_14, primals_15, primals_16, primals_17):
-        convolution = torch.ops.aten.convolution(primals_17, primals_8, primals_7, [4, 4], [2, 2], [1, 1], False, [0, 0], 1);  primals_7 = None
-        relu_ = torch.ops.aten.relu_(convolution);  convolution = None
-        max_pool2d_with_indices = torch.ops.aten.max_pool2d_with_indices(relu_, [3, 3], [2, 2])
-        getitem = max_pool2d_with_indices[0]
-        getitem_1 = max_pool2d_with_indices[1];  max_pool2d_with_indices = None
-        convolution_1 = torch.ops.aten.convolution(getitem, primals_12, primals_11, [1, 1], [2, 2], [1, 1], False, [0, 0], 1);  primals_11 = None
-        relu__1 = torch.ops.aten.relu_(convolution_1);  convolution_1 = None
-        max_pool2d_with_indices_1 = torch.ops.aten.max_pool2d_with_indices(relu__1, [3, 3], [2, 2])
-        getitem_2 = max_pool2d_with_indices_1[0]
-        getitem_3 = max_pool2d_with_indices_1[1];  max_pool2d_with_indices_1 = None
-        convolution_2 = torch.ops.aten.convolution(getitem_2, primals_14, primals_13, [1, 1], [1, 1], [1, 1], False, [0, 0], 1);  primals_13 = None
-        relu__2 = torch.ops.aten.relu_(convolution_2);  convolution_2 = None
-        convolution_3 = torch.ops.aten.convolution(relu__2, primals_16, primals_15, [1, 1], [1, 1], [1, 1], False, [0, 0], 1);  primals_15 = None
-        relu__3 = torch.ops.aten.relu_(convolution_3);  convolution_3 = None
-        convolution_4 = torch.ops.aten.convolution(relu__3, primals_10, primals_9, [1, 1], [1, 1], [1, 1], False, [0, 0], 1);  primals_9 = None
-        relu__4 = torch.ops.aten.relu_(convolution_4);  convolution_4 = None
-        max_pool2d_with_indices_2 = torch.ops.aten.max_pool2d_with_indices(relu__4, [3, 3], [2, 2])
-        getitem_4 = max_pool2d_with_indices_2[0]
-        getitem_5 = max_pool2d_with_indices_2[1];  max_pool2d_with_indices_2 = None
-        _adaptive_avg_pool2d = torch.ops.aten._adaptive_avg_pool2d(getitem_4, [6, 6])
-        view = torch.ops.aten.view(_adaptive_avg_pool2d, [128, 9216]);  _adaptive_avg_pool2d = None
-        t = torch.ops.aten.t(primals_2);  primals_2 = None
-        addmm = torch.ops.aten.addmm(primals_1, view, t);  primals_1 = None
-        relu__5 = torch.ops.aten.relu_(addmm);  addmm = None
-        t_1 = torch.ops.aten.t(primals_4);  primals_4 = None
-        addmm_1 = torch.ops.aten.addmm(primals_3, relu__5, t_1);  primals_3 = None
-        relu__6 = torch.ops.aten.relu_(addmm_1);  addmm_1 = None
-        t_2 = torch.ops.aten.t(primals_6);  primals_6 = None
-        addmm_2 = torch.ops.aten.addmm(primals_5, relu__6, t_2);  primals_5 = None
-        return [addmm_2, primals_16, relu__1, primals_17, t_1, relu__3, getitem_5, primals_8, relu__6, relu_, primals_10, t, view, getitem_4, t_2, getitem, primals_12, getitem_3, getitem_2, relu__4, getitem_1, relu__2, relu__5, primals_14]
+def f2(y):
+    x = y
+    for _ in range(5):
+        x = x.cos()
+    b = x.sum()
+    b.backward()
+    
+    return y.grad
 
 
-# m = FxModule()
-# inp = pickle.load(open("/scratch/shangdiy/work/torchbenchmark/torch_bench_graphs/alexnet/alexnet_forward_0/alexnet_forward_0.exampleinput", "rb"))
-# profile_module("alexnet_forward", m, inp)
+inp = torch.randn(2**20, device='cuda', generator=g_gpu, requires_grad=True)
+
+
+profile_function("joint-f2", f2, inp)
