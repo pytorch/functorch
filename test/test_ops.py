cc112d1a180e8064a27a715c004ecbc0626a2ebc
samdow <samdow@fb.com>
2022-07-05 10:43:58 -0400

    fix ci

--- test/test_ops.py
+++ test/test_ops.py
@@ -591,8 +591,6 @@ class TestOperators(TestCase):
         xfail('eig'),  # calls aten::item
         xfail('linalg.eig'),  # Uses aten::allclose
         xfail('linalg.householder_product'),  # needs select_scatter
-        xfail('linalg.slogdet'),  # calls .item()
-        xfail('logdet'),  # calls .item()
         xfail('matrix_exp'),  # would benefit from narrow_scatter
         xfail('nanquantile'),  # checks q via a .item() call
         xfail('nn.functional.gaussian_nll_loss'),  # checks var for if any value < 0
@@ -762,7 +760,6 @@ class TestOperators(TestCase):
         xfail('nn.functional.feature_alpha_dropout', 'without_train'),
         xfail('linalg.lu_factor', ''),
         xfail('nn.functional.dropout2d', ''),
-        xfail('nn.functional.kl_div', ''),
         xfail('pca_lowrank', ''),
         xfail('svd_lowrank', ''),
         xfail('linalg.lu_factor_ex', ''),
@@ -881,7 +878,6 @@ class TestOperators(TestCase):
         xfail('linalg.tensorsolve'),
         xfail('linalg.lu_factor', ''),
         xfail('nn.functional.feature_alpha_dropout', 'with_train'),
-        xfail('nn.functional.kl_div', ''),
         xfail('pca_lowrank', ''),
         xfail('nn.functional.dropout2d', ''),
         xfail('nn.functional.feature_alpha_dropout', 'without_train'),
@@ -1043,9 +1039,7 @@ class TestOperators(TestCase):
         xfail('cdist', ''),
         xfail('cholesky', ''),
         xfail('eig', ''),
-        xfail('linalg.slogdet', ''),
         xfail('logcumsumexp', ''),
-        xfail('logdet', ''),
         xfail('nn.functional.embedding_bag', ''),
         xfail('nn.functional.grid_sample', ''),
         xfail('nn.functional.hardsigmoid', ''),
@@ -1057,7 +1051,6 @@ class TestOperators(TestCase):
         xfail('renorm', ''),
         xfail('symeig', ''),
         xfail('nn.functional.feature_alpha_dropout', 'with_train'),
-        xfail('nn.functional.kl_div', ''),
         xfail('pca_lowrank', ''),
         xfail('nn.functional.dropout2d', ''),
         xfail('nn.functional.feature_alpha_dropout', 'without_train'),
