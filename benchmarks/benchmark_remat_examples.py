f127556a49200063c2c99aec7d41d6c75457ced4
Shangdi Yu <yushangdi123@gmail.com>
2022-07-08 19:04:11 +0000

    add eager time

--- benchmarks/benchmark_remat_examples.py
+++ benchmarks/benchmark_remat_examples.py
@@ -7,7 +7,7 @@ import copy
 
 from functorch._src.remat_utils_mincut import rematerialize, get_fused_graph
 from functorch._src.compile_utils import strip_overloads, fx_graph_cse
-from benchmark_remat_utils import profile_scripted_graph, profile_fused_graph
+from benchmark_remat_utils import profile_scripted_graph, profile_fused_graph, benchmark_GPU_time
 from functorch.compile import default_decompositions
 
 # Profile rematerialization algorithm on some hand-crafted examples
@@ -28,17 +28,18 @@ def profile_rematerialize(traced_graph, inp, list_inp):
 
 def profile_function(name, f, inp, list_inp = False):
     traced_graph = make_fx(f, decomposition_table=default_decompositions)(inp)
-    strip_overloads(traced_graph)
 
+    eager_time = benchmark_GPU_time(traced_graph, inp, list_inp)
     avg_cuda_time_f = profile_scripted_graph(traced_graph, inp, list_inp)
 
+    strip_overloads(traced_graph)
     csed = fx_graph_cse(traced_graph.graph)
     csed_graph =  fx.GraphModule(traced_graph, csed)
     csed_graph_copy = copy.deepcopy(csed_graph)
 
     avg_cuda_time_g, num_fused_group = profile_fused(csed_graph, inp, list_inp)
     avg_cuda_time_h, _ = profile_rematerialize(csed_graph_copy, inp, list_inp)
-    print(f"{name}, {avg_cuda_time_f}, {avg_cuda_time_g}, {avg_cuda_time_h}, {num_fused_group}", flush=True)
+    print(f"{name}, {eager_time}, {avg_cuda_time_f}, {avg_cuda_time_g}, {avg_cuda_time_h}, {num_fused_group}", flush=True)
 
 
 def f(a):
@@ -75,7 +76,7 @@ def f2(y):
 
 def benchmark_on_small_examples():
     random.seed(1)
-    print("name, scripted_cuda_time, fused_cuda_time, remat_cuda_time, num_fusion_group", flush=True)
+    print("name, eager_time, scripted_cuda_time, fused_cuda_time, remat_cuda_time, num_fusion_group", flush=True)
     g_gpu = torch.Generator(device='cuda')
     g_gpu.manual_seed(214748364)
     inp = torch.randn(2**20, device='cuda', generator=g_gpu)
