8e2f53b69405970ddeea7ad984e4d4c8c7c499c6
Richard Zou <zou3519@gmail.com>
2022-09-29 14:08:29 -0700

    Preparations for the upcoming release

--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -12,5 +12,14 @@ python -m torch.utils.collect_env
 # test_functorch_lagging_op_db.py: Only run this locally because it checks
 # the functorch lagging op db vs PyTorch's op db.
 EXIT_STATUS=0
+
+git_version=$(python -c "import torch.version; print(torch.version.git_version)")
+echo git_version
+
+git clone https://github.com/pytorch/pytorch.git
+pushd pytorch
+git checkout $git_version
+pushd functorch
+
 find test \( -name test\*.py ! -name test_functorch_lagging_op_db.py \) | xargs -I {} -n 1 python {} -v || EXIT_STATUS=$?
 exit $EXIT_STATUS
