dc18feb5c5e2fe972d70549651e48d6aebe3c63c
pytorchbot <soumith+bot@pytorch.org>
2025-07-15 00:54:47 +0000

    Generate Python docs from pytorch/pytorch@7e433d5f423248914c5e9838d3ea145db7964923

--- nightly/_static/js/theme.js
+++ nightly/_static/js/theme.js
@@ -216,37 +216,33 @@ function openGitHubIssue() {
 document.addEventListener('DOMContentLoaded', function() {
   var downloadNote = $(".sphx-glr-download-link-note.admonition.note");
   if (downloadNote.length >= 1) {
-    var tutorialUrlArray = $("#tutorial-type").text().split('/');
-    tutorialUrlArray[0] = tutorialUrlArray[0] + "_source";
+      var tutorialUrlArray = $("#tutorial-type").text().split('/');
+      tutorialUrlArray[0] = tutorialUrlArray[0] + "_source"
 
-    var githubLink = "https://github.com/pytorch/tutorials/blob/main/" + tutorialUrlArray.join("/") + ".py";
+      var githubLink = "https://github.com/pytorch/tutorials/blob/master/" + tutorialUrlArray.join("/") + ".py";
 
-    // Find the notebook download link - in pydata sphinx theme the structure might be different
-    var notebookLinks = $(".reference.download");
-    var notebookLink = notebookLinks.length > 1 ? notebookLinks[1].href : notebookLinks[0].href;
+      // Find the notebook download link by checking for .ipynb extension
+      var notebookLinks = $(".reference.download");
+      var notebookLink = "";
+      for (var i = 0; i < notebookLinks.length; i++) {
+          if (notebookLinks[i].href.endsWith('.ipynb')) {
+              notebookLink = notebookLinks[i].href;
+              break;
+          }
+      }
+      // Fallback to first link if no .ipynb found
+      if (!notebookLink && notebookLinks.length > 0) {
+          notebookLink = notebookLinks[0].href;
+      }
 
-    var notebookDownloadPath = notebookLink.split('_downloads')[1];
-    var colabLink = "https://colab.research.google.com/github/pytorch/tutorials/blob/gh-pages/_downloads" + notebookDownloadPath;
+      var notebookDownloadPath = notebookLink.split('_downloads')[1],
+          colabLink = "https://colab.research.google.com/github/pytorch/tutorials/blob/gh-pages/_downloads" + notebookDownloadPath;
 
-    // Create the links directly instead of wrapping existing elements
-    var colabElement = $("#google-colab-link");
-    if (colabElement.length) {
-      colabElement.find('div').wrapInner('<a href="' + colabLink + '" data-behavior="call-to-action-event" data-response="Run in Google Colab" target="_blank"></a>');
-    }
-
-    var notebookElement = $("#download-notebook-link");
-    if (notebookElement.length) {
-      notebookElement.find('div').wrapInner('<a href="' + notebookLink + '" data-behavior="call-to-action-event" data-response="Download Notebook"></a>');
-    }
-
-    var githubElement = $("#github-view-link");
-    if (githubElement.length) {
-      githubElement.find('div').wrapInner('<a href="' + githubLink + '" data-behavior="call-to-action-event" data-response="View on Github" target="_blank"></a>');
-    }
-    $(".sphx-glr-footer").hide();
-    $(".sphx-glr-signature").hide();
+      $("#google-colab-link").wrap("<a href=" + colabLink + " data-behavior='call-to-action-event' data-response='Run in Google Colab' target='_blank'/>");
+      $("#download-notebook-link").wrap("<a href=" + notebookLink + " data-behavior='call-to-action-event' data-response='Download Notebook'/>");
+      $("#github-view-link").wrap("<a href=" + githubLink + " data-behavior='call-to-action-event' data-response='View on Github' target='_blank'/>");
   } else {
-    $(".pytorch-call-to-action-links").hide();
+      $(".pytorch-call-to-action-links").hide();
   }
 });
 ;$(document).ready(function() {
